e11881eb359234590d6fce2d07803136e6d7bd77
dlutwuwei <dlutwuwei@qq.com>
2016-11-05 22:39:06 +0800

    modify login page

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -19,38 +19,37 @@ var login = React.createClass({
 		}.bind(this));
 	},
 	photoin() {
-		var self = this;
-		var options = true;
+		let self = this;
+		let options = true;
 		if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
 			try {
 				navigator.mediaDevices.enumerateDevices()
-					.then(function (devices) {
-						devices.forEach(function (device,index) {
+					.then((devices) => {
+						devices.forEach( (device, index) => {
 							if (device.kind === 'videoinput') {
-								if(device.label.toLowerCase().indexOf("back") > 0){
+								if (device.label.toLowerCase().indexOf('back') > 0) {
 									options = { 'deviceId': { 'exact': device.deviceId }, 'facingMode': 'environment',label: device.label};
 								}
-								console.log(device.deviceId,device.label)
+								console.log(device.deviceId, device.label);
 							}
 						});
-						webcam(options)
+						webcam(options);
 					});
-
-
-
-			}
-			catch (e) {
+			} catch (e) {
 				alert(e);
 			}
-		}
-		else {
+		} else {
 			webcam(options);
 			console.log("no navigator.mediaDevices.enumerateDevices");
 		}
 
+		function noStream(e) {
+			alert('no camera' + e);
+		}
+
 		function webcam(options) {
 			navigator.getum = navigator.webkitGetUserMedia || navigator.getUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-			console.log(options)
+			console.log(options);
 			navigator.getum({
 				video: options,
 				audio: false
@@ -58,24 +57,20 @@ var login = React.createClass({
 		}
 
 		function gotStream(stream) {
-			if(window.URL.createObjectURL) this.video.src = window.URL.createObjectURL(stream);
+			if (window.URL.createObjectURL) this.video.src = window.URL.createObjectURL(stream);
 			else this.video.src = stream;
-			this.video.onerror = function () {
+			this.video.onerror = () => {
 				stream.stop();
 			};
 			stream.onended = noStream;
-			this.video.onloadedmetadata = function () {
-				alert('ok')
+			this.video.onloadedmetadata = () => {
+				console.log('ok');
 			};
-		}
-
-		function noStream(e) {
-			alert('no camera'+e);
 		}
 	},
 	snapshot() {
 		this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-		qrcode.decode();
+		//qrcode.decode();
 	},
 	componentDidMount() {
 		this.canvas = document.getElementById('qr-canvas');
@@ -87,16 +82,21 @@ var login = React.createClass({
 				<h1>登陆</h1>
 				<section id="app">
 					<p>
-						<video id="monitor" className='monitor' autoPlay></video>
-						<canvas id="qr-canvas" className='photo'></canvas>
+						<video id="monitor" className="monitor" autoPlay></video>
 					</p>
 				</section>
-				<span>输入用户code：</span>
-				<div className='content'>
-					<input id='code' type='text'/>
-					<button onClick={this.signin}>登陆</button>
-					<button onClick={this.photoin}>二维码</button>
-					<button onClick={this.snapshot}>识别</button>
+				<span>输入用户access-code：</span>
+				<div className="content">
+					<input id="code" type="text"/>
+					<button onClick={this.signin} className="am-btn-primary">登陆</button>
+				</div>
+				<div className="content">
+					<button onClick={this.photoin} className="am-btn-primary">开启摄像头</button>
+					<button onClick={this.snapshot} className="am-btn-primary">拍照识别</button>
+					<button onClick={this.snapshot} className="am-btn-primary">文件识别</button>
+				</div>
+				<div className="monitor">
+				<canvas id="qr-canvas" className="photo"></canvas>
 				</div>
 			</div>
 		);
